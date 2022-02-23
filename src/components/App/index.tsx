30b4e8f65866ce9456ea1f9d42c2ebdec6ef3d43
Nathan nascimento <nathasilvanascimento@gmail.com>
2022-02-23 18:56:21 -0300

    subindo o projeto

---
+++ src/components/App/index.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+import { Routes } from "../Routes/Routes";
+
+
+export function App() {
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  )
+}
--- src/components/Routes/Routes.tsx
+++ src/components/Routes/Routes.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { Route, Routes as RoutesRRD } from 'react-router-dom';
-import { lazyComponent } from '../../common/utils';
-
-const Home = lazyComponent('Home', import('../../pages/Home/Home'));
-const PageNotFound = lazyComponent('PageNotFound', import('../../pages/PageNotFound/PageNotFound'));
+import { Home } from '../../pages/Home/Home';
 
 export const Routes = (): JSX.Element => (
   <RoutesRRD>
     <Route element={<Home />} path="/" />
-    <Route element={<PageNotFound />} path="*" />
   </RoutesRRD>
 );
