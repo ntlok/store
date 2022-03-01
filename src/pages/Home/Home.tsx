683dd93a2462006eaa99adbb19bfe94255870d8a
Nathan nascimento <nathasilvanascimento@gmail.com>
2022-03-01 10:02:28 -0300

    mudando o background da home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,15 @@
 import react from "react";
 import { Link } from "react-router-dom";
 
+import '../Home/Home.scss'
+
 export const Home = () => {
   return (
-    <>
+    <div className="home-page" >
       <h1>Clique para ir para pagina</h1>
       <Link to="/page">
         clique aqui
       </Link>
-    </>
+    </div>
   )
 }
